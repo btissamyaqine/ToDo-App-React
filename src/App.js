0c1c6b8b6087280422effbcf9e6caf212676d43b
Btissam Yaqine <btissamyaqine123@gmail.com>
2020-12-19 20:09:29 +0100

    add ToDo-App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import './App.css';
+//Importing Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+      <h1> Ed's ToDo List</h1>
+      </header> 
+      <Form 
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos}  
+      setInputText={setInputText}
+      /> 
+      <TodoList setTodos={setTodos} todos={todos}/>  
+    </div>
+  );
+}
+
+export default App;
